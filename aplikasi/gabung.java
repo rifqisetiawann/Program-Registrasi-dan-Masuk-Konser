05e8da5a7c77103df9393a03f8428742a0e461f5
rifqisetiawann <76649721+rifqisetiawann@users.noreply.github.com>
2020-12-27 21:17:28 +0700

    Add files via upload

---
+++ aplikasi/gabung.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class gabung {
+    
+    public static void main(String[] args) {
+        regis rgs = new regis();
+        rgs.setVisible(true);
+        rgs.pack();
+        rgs.setLocationRelativeTo(null);
+        rgs.setDefaultCloseOperation(regis.EXIT_ON_CLOSE);
+    }
+    
+}
